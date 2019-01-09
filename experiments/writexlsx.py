e742f866fc1a8c226a0108962528f0aece9c1562
gprok <gprok60@gmail.com>
2019-01-09 11:43:00 +0200

    Refactored xlsx writer to write any data

--- experiments/writexlsx.py
+++ experiments/writexlsx.py
@@ -1,19 +1,41 @@
 import xlsxwriter
 
-workbook = xlsxwriter.Workbook('Expenses01.xlsx')
-worksheet = workbook.add_worksheet()
-expenses = (
-   ['Rent', 1000],
-   ['Gas',   100],
-   ['Food',  300],
-   ['Gym',    50],
-)
-row = 0
-col = 0
-for item, cost in (expenses):
-   worksheet.write(row, col, item)
-   worksheet.write(row, col + 1, cost)
-   row += 1
-worksheet.write(row, 0, 'Total')
-worksheet.write(row, 1, '=SUM(B1:B4)')
-workbook.close()
+class XlsxCreator:
+    """ Creates a xslx file and writes data into it """
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def setData(self, data):
+        """ Expects a list of lists (data) representing spreadsheet rows """
+        self.data = data
+
+    def createSpreadsheet(self):
+        workbook = xlsxwriter.Workbook(self.filename + '.xlsx')
+        worksheet = workbook.add_worksheet()
+        rowcounter = 0
+        for row in self.data:
+            colcounter = 0
+            for item in row:
+                worksheet.write(rowcounter, colcounter, item)
+                colcounter += 1
+            rowcounter += 1
+        workbook.close()
+
+
+def main():
+    data = (
+        ['EXPENSE', 'VALUE'],
+        ['Rent', 1000],
+        ['Gas',   100],
+        ['Food',  300],
+        ['Gym',    50],
+        ['TOTAL', '=SUM(B2:B5)'],
+    )
+    spreadsheet = XlsxCreator("Expenses01")
+    spreadsheet.setData(data)
+    spreadsheet.createSpreadsheet()
+
+
+if __name__== "__main__":
+    main()
